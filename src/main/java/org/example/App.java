b4cd3f5e3cc17deb3dc611cadd6cb1c73c011a23
The-Popa <tobias.carlstrom@bredband2.com>
2020-04-07 11:01:53 +0200

    Initial commit

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,16 @@
+package org.example;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        String greeting = "Hello!";
+        String name = "Tobias";
+        System.out.println(greeting);
+        System.out.println(name);
+    }
+}
